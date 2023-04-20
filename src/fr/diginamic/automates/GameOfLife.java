9f30595add8aa166e66eeb1e75c0b2412a0f9610
S-Keyp <s-keyp@outlook.fr>
2023-04-20 17:01:01 +0200

    R

--- src/fr/diginamic/automates/GameOfLife.java
+++ src/fr/diginamic/automates/GameOfLife.java
@@ -1,26 +1,50 @@
 package fr.diginamic.automates;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Menu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameOfLife {
-	static int WIDTH = 50;
-	static int HEIGHT = 50;
+	static int WIDTH = 100;
+	static int HEIGHT = 100;
+
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Game of life");
-		frame.setSize(5 * WIDTH, 5 * HEIGHT);
 		frame.setVisible(true);
 		frame.setResizable(true);
 		
 		Grid grid = new Grid(WIDTH, HEIGHT);
-		Dimension dim = new Dimension(10 * WIDTH,10 * HEIGHT);
-		Dimension slot = new Dimension(10, 10);
+		Dimension dim = new Dimension(5 * WIDTH, 5 * HEIGHT);
+		Dimension slot = new Dimension(5, 5);
 		FrameCanvas canvas = new FrameCanvas(dim, slot, grid);
-		frame.add(canvas);
+	
+		JPanel menu = new JPanel();
+		menu.setPreferredSize(new Dimension(150, 300));
+		menu.setBackground(Color.RED);
+
+		JButton startStop = new JButton("Start / Stop");
+		startStop.setBackground(Color.GREEN);
+		menu.add(startStop,BorderLayout.CENTER);
+		
+		
+		// JButton tchoin = new JButton("tchoin");
+		// tchoin.setBackground(Color.GREEN);
+		// menu.add(tchoin,BorderLayout.CENTER);
+
+
+		frame.add(menu, BorderLayout.EAST);
+		frame.add(canvas, BorderLayout.WEST);
 		frame.pack();
 
 		for(;;){
 			canvas.play();
-
 			try{
 				Thread.sleep(60);
 			}catch(Exception e){
